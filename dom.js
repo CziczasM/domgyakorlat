e50e5dffd85fe03bbe800f496ea54b6b13495811
CziczasM <cziczas.martin@kreativszolnok.hu>
2025-11-13 11:23:57 +0100

    működik a p

--- dom.js
+++ dom.js
@@ -8,3 +8,16 @@ function ujDivHozzaad() {
     container.append(ujDiv)
     szamlalo=szamlalo+1
 }
+function addP() {
+    const ujP = document.createElement("p");
+    ujP.innerHTML = "Új bekezdés szövege";
+    const cont = document.getElementById("container");
+    const divek = cont.getElementsByTagName("div");
+    const utolso_div = divek[divek.length-1]
+    
+    if (divek.length>0) {
+        utolso_div.append(ujP);
+    }else{
+        alert("Először divet hozz létre!")
+    }
+}
